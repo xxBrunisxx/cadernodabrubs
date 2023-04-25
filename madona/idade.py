fb4fdf82a338bb647896bd871463caef7da3b508
xxBrunisxx <131306196+xxBrunisxx@users.noreply.github.com>
2023-04-25 15:58:10 -0300

    Add files via upload

---
+++ madona/idade.py
@@ -0,0 +1,7 @@
+print ("qual sua idade?")
+idade=input()
+x=int(idade)
+if x >=18:
+        print("você é maior de idade")
+if x <18:
+        print("você é menor de idade")
---
+++ madona/somador.py
@@ -0,0 +1,6 @@
+num1=input("digite um numero:")
+num2=input("digite outro numero:")
+x=int(num1)
+y=int(num2)
+resultado=x+y
+print(resultado)
